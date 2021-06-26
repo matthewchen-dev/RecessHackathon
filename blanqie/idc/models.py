226b52f7058670b5bc76b852e98ccaef47732417
syedmza <alam.syedmz@gmail.com>
2021-06-26 19:58:16 -0400

    nothing?

--- blanqie/idc/models.py
+++ blanqie/idc/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Doc(models.Model):
+    upload = models.FileField(upload_to='text/')
+    name = models.CharField(max_length = 15, default = "notes")
+    def __str__(self):
+        return str(self.pk)
--- blanqie/idc/views.py
+++ blanqie/idc/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .models import Doc
+from rake_nltk import Rake
 
 # Create your views here.
 
@@ -12,3 +16,30 @@ def learn(request):
 
 def test(request):
     return render(request, 'idc/test.html')
+
+class MainView(TemplateView):
+    template_name = 'idc/test.html'
+
+def file_upload_view(request):
+    #print(request.FILES)
+
+    if request.method == 'POST':
+        my_file = request.FILES.get('file')
+        Doc.objects.create(name = 'notes', upload = my_file)
+        return HttpResponse('upload')
+    return JsonResponse({'post': 'false'})
+
+all_entries = Doc.objects.last().upload
+all_entries.open(mode = 'r+')
+lines = all_entries.read()
+
+
+r = Rake() # Uses stopwords for english from NLTK, and all puntuation characters.
+r = Rake(min_length=1, max_length=2)
+
+print(r.extract_keywords_from_text(lines))
+print(r.get_ranked_phrases()) # To get keyword phrases ranked highest to lowest.
+print(r.get_ranked_phrases_with_scores())
+
+
+all_entries.close()
