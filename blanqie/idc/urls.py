fd1c484df10067049dfdf8ef36e8c47500792183
syedmza <alam.syedmz@gmail.com>
2021-06-27 03:58:03 -0400

    landing page done

--- blanqie/idc/urls.py
+++ blanqie/idc/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('learn/', views.learn),
-    path('test/', views.test),
+    path('', views.home, name="home"),
+    path('learn/', views.learn, name="learn"),
+    path('test/', views.test, name="test"),
     path('test/upload/', views.file_upload_view)
 ]
