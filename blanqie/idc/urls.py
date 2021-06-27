0046a4e2ccc648d43b9877d6a77639aaef048d00
matthewchen-dev <matthew.lk.chen@gmail.com>
2021-06-27 04:26:06 -0400

    Hopefully

--- blanqie/idc/urls.py
+++ blanqie/idc/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('learn/', views.learn, name="learn"),
     path('test/', views.test, name="test"),
-    path('test/upload/', views.file_upload_view)
+    path('test/upload/', views.file_upload_view),
+    path('test/dl/', views.download),
 ]
